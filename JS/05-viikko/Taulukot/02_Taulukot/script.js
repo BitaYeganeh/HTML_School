6c8b941579fcdf43895a6a6a92869e72e3ba8552
Bita Yeganeh <b.yeganeh85@gmail.com>
2025-04-23 14:54:57 +0300

    wip

---
+++ JS/05-viikko/Taulukot/02_Taulukot/script.js
@@ -0,0 +1,14 @@
+// Harjoitus 1: Määritä taulukon pituus ja tarkista ehto
+let myAlphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+/*
+Luo funktio nimeltä myAlphabetLength, joka tulostaa myAlphabet-taulukon pituuden.
+Jos pituus on 5 tai enemmän, tulosta: "Taulukon pituus on riittävä".
+Muussa tapauksessa tulosta: "Taulukon pituus on liian lyhyt."
+*/
+// Kirjoita funktiosi tähän
+
+// Harjoitus 2: Käy läpi planets-taulukko ja tulosta jokainen planeetta ja sen indeksi
+const planets = ["Merkurius", "Venus", "Maa", "Mars", "Jupiter"];
+
+//
