b013651718e8fc0de1b0da3ff101e330f5cb81a4
Bita Yeganeh <b.yeganeh85@gmail.com>
2025-04-24 14:54:37 +0300

    wip

---
+++ JS/05-viikko/Taulukot/02_Taulukot/Harjoitus 9/script.js
@@ -0,0 +1,7 @@
+// Harjoitus 9: Yritä etsiä eläin, jota ei ole zooAnimals-taulukossa
+/*
+Etsi "Pingviini":n indeksi zooAnimals-taulukosta.
+Jos sitä ei löydy, tulosta "Pingviini ei ole eläintarhassa."
+Odotettu tulos: -1
+*/
+// Kirjoita koodisi tähän
