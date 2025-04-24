64e9c9c8b40d5d317233c990591ba2e058ddafe5
BitaYeganeh <b.yeganeh85@gmail.com>
2025-04-24 21:29:39 +0300

    Delete My projects directory

--- My projects/lamp project/script.js
+++
@@ -1,9 +0,0 @@
-
-function changeImage() {
-    var image = document.getElementById('OnOffLamp');
-    if (image.src.match("bulbon")) {
-      image.src = "./images/pic_bulboff.gif";
-    } else {
-      image.src = "./images/pic_bulbon.gif";
-    }
-  }
