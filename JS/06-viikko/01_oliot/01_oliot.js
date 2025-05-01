3a127e3de44d2416270a4a137153eace92a40725
Bita Yeganeh <b.yeganeh85@gmail.com>
2025-05-01 19:11:54 +0300

    wip

--- JS/06-viikko/01_oliot/01_oliot.js
+++ JS/06-viikko/01_oliot/01_oliot.js
@@ -4,8 +4,6 @@ Jokaisella kirjalla tulee olla ominaisuudet `title` (otsikko), `author` (kirjoit
 Lisää kokoelmaan vähintään kaksi kirjaoliota.
 */
 
-// Kirjoita koodisi tähän
-
 const library = [ 
     {
         title:"Kuolema ja Pingviini", 
@@ -22,16 +20,12 @@ const library = [
         author:"townsend, Jessica", 
         yearPublished:2018 
     }
-
 ];
-
 
 /* Tehtävä 2
 Hae `library`-kokoelman ensimmäisen kirjan `title` ja tulosta se konsoliin.
 Muokkaa toisen kirjan `yearPublished` uudeksi vuodeksi ja tulosta päivitetty kirja konsoliin.
 */
-
-// Kirjoita koodisi tähän
 
 console.log("Ensimmäisen kirjan nimi:", library[0].title);
 library[1].yearPublished = 2025;
@@ -43,11 +37,11 @@ Käytä hakasulkunotaatiota lisätäksesi boolean-tyyppisen ominaisuuden `isAvai
 */
 // Kirjoita koodisi tähän
 
-library[0].genres = ["Absurdi", "Tragikomiikk"];
-console.log(library[0]);
+library[0].genres = ["Absurdi", "Tragikomiikka"];
 
+library[0]["isAvailable"] =true;
 
-
+console.log("onko kirja saatavilla?", library[0]["isAvailable"]);
 
 
 /* Tehtävä 4
@@ -57,6 +51,17 @@ Luo uusi kirjaolio `Book`-konstruktorilla ja lisää se `library`-kokoelmaan.
 
 // Kirjoita koodisi tähän
 
+function Book(title, author, yearPublished, genres){
+    this.title = title;
+    this.author =author;
+    this.yearPublished=yearPublished;
+    this.genres = genres;
+
+}
+const newBook = new Book("Why We Sleep", "Matthew Walker", 1992, ["Unihäiriöt"]);
+library.push(newBook);
+console.log(library);
+
 /* Tehtävä 5
 Kirjoita funktio nimeltä `createBook`, joka ottaa parametreina `title`, `author`, `yearPublished` ja `genres` (taulukko).
 Funktion tulee palauttaa uusi kirjaolio näillä ominaisuuksilla.
@@ -65,12 +70,35 @@ Testaa `createBook`-funktiota luomalla uusi kirja käyttäjän antamilla arvoill
 
 // Kirjoita koodisi tähän
 
+function createBook (title, author, yearPublished, genres){
+
+this.title=title;
+this.author=author;
+this.yearPublished=yearPublished;
+this.genres=genres;
+
+}
+
+
+
+
+
+
+
 /* Tehtävä 6
 Muunna `library`-kokoelma JSON-merkkijonoksi ja tulosta se konsoliin.
 Jäsennä JSON-merkkijono takaisin JavaScript-olioksi ja tulosta ensimmäisen kirjan `title` konsoliin.
 */
 
 // Kirjoita koodisi tähän
+
+
+
+
+
+
+
+
 
 /* Tehtävä 7
 Luo taulukko nimeltä `students`, jossa jokaisella oppilaalla on `name`, `age` ja `grade`.
@@ -79,12 +107,32 @@ Tulosta niiden oppilaiden nimet, joiden arvosana on yli 90.
 
 // Kirjoita koodisi tähän
 
+
+
+
+
+
+
+
+
+
 /* Tehtävä 8
 Määrittele olio nimeltä `car`, jossa on ominaisuudet `brand`, `model`, `year` ja `isElectric`.
 Käytä funktiota tarkistamaan onko auto sähköinen. Jos on, tulosta `"This car is eco-friendly!"`, muussa tapauksessa `"This car runs on fuel."`
 */
 
 // Kirjoita koodisi tähän
+
+
+
+
+
+
+
+
+
+
+
 /* Tehtävä 9
 Luo `movies`-taulukko, jossa jokainen elokuva on olio, jolla on ominaisuudet `title`, `director` ja `rating`.
 Kirjoita silmukka, joka tulostaa kaikkien niiden elokuvien otsikot, joiden arvio on yli 8.
@@ -92,11 +140,29 @@ Kirjoita silmukka, joka tulostaa kaikkien niiden elokuvien otsikot, joiden arvio
 
 // Kirjoita koodisi tähän
 
+
+
+
+
+
+
+
+
+
 /* Tehtävä 10
 Kirjoita funktio `findOldestCar`, joka ottaa vastaan taulukon auto-olioita ja palauttaa auton, jolla on varhaisin `year`.
 */
 
 // Kirjoita koodisi tähän
+
+
+
+
+
+
+
+
+
 
 /* Tehtävä 11
 Määrittele `userProfiles`-taulukko, joka sisältää käyttäjäolioita.
@@ -106,12 +172,31 @@ Kirjoita funktio, joka suodattaa ja palauttaa vain ne käyttäjät, jotka ovat y
 
 // Kirjoita koodisi tähän
 
+
+
+
+
+
+
+
+
+
+
 /* Tehtävä 12
 Määrittele taulukko nimeltä `orders`, jossa jokaisella tilauksella on `orderId`, `customerName`, `totalAmount` ja `status` ("pending" tai "completed").
 Kirjoita funktio, joka palauttaa vain valmiit tilaukset.
 */
 
 // Kirjoita koodisi tähän
+
+
+
+
+
+
+
+
+
 
 /* Tehtävä 13
 Luo olio nimeltä `smartphone`, jolla on ominaisuudet `brand`, `model`, `batteryLife` ja `is5GEnabled`.
@@ -120,12 +205,33 @@ Kirjoita funktio, joka tulostaa `"This phone supports 5G!"` jos `is5GEnabled` on
 
 // Kirjoita koodisi tähän
 
+
+
+
+
+
+
+
+
+
+
 /* Tehtävä 14
 Määrittele olio nimeltä `fox`, jolla on ominaisuudet `name`, `age` ja `habitat`.
 Kirjoita funktio, joka tulostaa `"This fox is young"` jos ikä on alle 3, ja `"This fox is an adult"` jos 3 tai enemmän.
 */
 
 // Kirjoita koodisi tähän
+
+
+
+
+
+
+
+
+
+
+
 
 /* Tehtävä 15
 Määrittele taulukko nimeltä `employees`, jossa jokaisella työntekijällä on `name`, `position` ja `salary`.
