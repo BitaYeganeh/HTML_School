961cb43a12be32b052124b997e62fa6f53b015ad
Bita Yeganeh <b.yeganeh85@gmail.com>
2025-05-05 21:53:32 +0300

    wip

--- JS/07-viikko/03_olioit/script.js
+++ JS/07-viikko/03_olioit/script.js
@@ -4,14 +4,45 @@ Sen tulee luoda olio, jolla on nämä ominaisuudet sekä metodi `showInfo`, joka
 Luo uusi käyttäjä ja kutsu `showInfo()`.
 */
 
-// Kirjoita koodisi tähän
+function User(username, email) {
+this.username = username;
+this.email = email;
+
+this.showInfo = function () {
+    console.log(`Username: ${this.username}, Email: ${this.email}`);
+};
+}
+const user1 = new User ("Bita", "bita.yeganeh@gmail.com");
+user1.showInfo();
 
 /* Tehtävä 2
 Muokkaa tehtävää 1: Käytä ES6 `class`-syntaksia `User`-luokan määrittelyyn.
 Lisää metodi `changeEmail(newEmail)`, joka päivittää käyttäjän sähköpostiosoitteen.
 */
 
-// Kirjoita koodisi tähän
+class User1{
+    constructor(username, email) {
+        this.username = username;
+        this.email = email;
+    }
+    showInfo() {
+        console.log(`username: ${this.username}, Email: ${this.email}`);
+    }
+
+    changeEmail(newEmail){
+        this.email = newEmail;
+        console.log(`Email is updated to : ${this.email}`);
+    }
+}
+
+// add a new user to test it:
+const user3 = new User1("Bita", "b.yeganeh@gmail.com");
+user3.showInfo();
+// change email:
+user3.changeEmail("gh.yahoo.com");
+user3.showInfo();
+
+
 
 /* Tehtävä 3
 Luo olio `theme`, jolla on ominaisuudet `name` (light tai dark) ja `isActive`.
