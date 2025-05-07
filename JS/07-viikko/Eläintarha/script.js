d9ad9992ecc3668730e6e1bf9a7c88a2fe03f369
Bita Yeganeh <b.yeganeh85@gmail.com>
2025-05-07 23:11:34 +0300

    wip

--- JS/07-viikko/Eläintarha/script.js
+++ JS/07-viikko/Eläintarha/script.js
@@ -1,5 +1,30 @@
 const eläinlista = [
     { name: "Kettu", type: "Nisäkäs" },
     { name: "Karhu", type: "Nisäkäs" },
+    { name: "Kotka", type: "Lintu" },
+    { name: "Sammakko", type: "Sammakkoeläin" },
+    { name: "Käärme", type: "Matelija" },
+    { name: "Hai", type: "Kala" },
+    { name: "Leijona", type: "Nisäkäs" },
+    { name: "Papukaija", type: "Lintu" },
+    { name: "Kilpikonna", type: "Matelija" },
+    { name: "Tursas", type: "Nilviäinen" },
+    { name: "Ampiainen", type: "Hyönteinen" },
+    { name: "Pöllö", type: "Lintu" },
 ];
+// 2. Funktio listan näyttämiseen
+function showAnimals(list) {
+    const animalListElement = document.getElementById("animalList");
+    animalListElement.innerHTML ="" // making the list empty first
 
+    list.forEach(animal => {
+        const li = document.createElement("li");
+        li.textContent =`${animal.name} ${animal.type}`;
+        animalListElement.appendChild(li);
+    });
+}
+
+//show the animlas when the page loads:
+document.addEventListener("DOMContentLoaded", ()=>{
+    showAnimals(eläinlista);
+});
